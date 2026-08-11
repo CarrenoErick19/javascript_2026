adf02274c4d2742e5b187f06d4dc622f3c2f8b3a
Erick Carreño Reyes <78943441+CarrenoErick19@users.noreply.github.com>
2026-08-11 16:07:47 -0500

    cambios hasta añadir tarea con sus botones

--- app_js_tareas/src/main.js
+++ app_js_tareas/src/main.js
@@ -1,17 +1,26 @@
 const tarea = document.getElementById("tarea"); //obtenemos el valor de tarea
-const lista = document.getElementById("listaTareas"); //obtenemos la lista "ul"
 const bt1 = document.getElementById("bt1"); //obtenemos el boton
+const accion = document.getElementById("accion");
 const idTarea = document.getElementById("idTarea");
+const Tareas = document.getElementById("tablaTareas")
 function llenarFormulario() {
-  let nuevaTarea = document.createElement('li'); //definimos nuevatarea que sera igual a un nuevo valor en la lista
-  nuevaTarea.textContent = tarea.value;  //asignamos a nueva tarea el valor llenado desde el input
-  idTarea.textContent = "001";
+  let nuevaFila = Tareas.insertRow(1);
+  let nuevoid = nuevaFila.insertCell(0)
+  let nuevaTarea = nuevaFila.insertCell(1)
+  let accion1 = nuevaFila.insertCell(2)
+  let accion2 = nuevaFila.insertCell(3)
+  nuevaTarea.textContent = tarea.value;
+  nuevoid.textContent = "001";
+  let botonEliminar = document.createElement('button');
+  accion1.appendChild(botonEliminar);
+  let checkTarea = document.createElement('input');
+  checkTarea.type = "checkbox"; 
+  accion2.appendChild(checkTarea); 
   let cadenaIf = tarea.value;
   if (cadenaIf == "") {
     alert("Por favor, ingrese un valor que no sea un espacio en blanco");
   }else{
     lista.appendChild(nuevaTarea); //se añade dicha nueva tarea al final de la lista
-    let botonEliminar = document.createElement('button');
   }
 }
 function clearInput() {
@@ -19,3 +28,6 @@ function clearInput() {
 }
 bt1.addEventListener("click", llenarFormulario); // al hacer clic se ejecuta la funcion para añadir una tarea
 bt1.addEventListener("click", clearInput)
+
+
+//leer documentacion sobre: Node.textContent, Node.appendChild, Element.append
